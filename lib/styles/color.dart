95daa0926c4b5aaeb9103915f7a052ba603c366a
민감자 <mouse0429@naver.com>
2023-11-06 01:48:24 +0900

    feat(color): 기본 컬러칩 추가

---
+++ lib/styles/color.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class KwangColor {
+  static const grey100 = Colors.white;
+  static const grey200 = Color(0xFFFBFBFB);
+  static const grey300 = Color(0xFFF4F6FA);
+  static const grey400 = Color(0xFFDFE3EC);
+  static const grey500 = Color(0xFFC3C8D0);
+  static const grey600 = Color(0xFFB4B9C2);
+  static const grey700 = Color(0xFF777C83);
+  static const grey800 = Color(0xFF464852);
+  static const grey900 = Color(0xFF303030);
+  static const black = Color(0xFF202020);
+  
+  static const primary100 = Color(0xFFE3EBE5);
+  static const primary200 = Color(0xFFA5B9AB);
+  static const primary300 = Color(0xFF587761);
+  static const primary400 = Color(0xFF3D5D47);
+
+  static const red = Color(0xFFEE3C32);
+  static const blue = Color(0xFF2260FF);
+  static const green = Color(0xFF6FC36D);
+  static const lightRed = Color(0xFFFAEAEA);
+  static const lightGreen = Color(0xFFE6F7E6);
+}
