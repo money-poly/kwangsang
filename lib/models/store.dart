aa85c13c62d281843ba4ed8ef13bacc219d66590
민감자 <mouse0429@naver.com>
2023-11-22 17:23:15 +0900

    feat(store): 가게 카드를 위한 가게 모델

--- lib/models/store.dart
+++ lib/models/store.dart
@@ -1,4 +1,5 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:immersion_kwangsang/models/store_tag.dart';
 
 class StoreSimple {
   int id;
@@ -6,4 +7,13 @@ class StoreSimple {
   LatLng latLng;
 
   StoreSimple({required this.id, required this.name, required this.latLng});
+}
+
+class Store {
+  String name;
+  String description;
+  int maxDiscountRate;
+  List<StoreTag> tags;
+
+  Store({required this.name, required this.description, required this.maxDiscountRate, required this.tags});
 }
