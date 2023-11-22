e883bed49145d5b137c6de750e53bb72017a811d
민감자 <mouse0429@naver.com>
2023-11-22 16:08:14 +0900

    feat(store): 마커 표시를 위한 가게 기본 정보 모델

---
+++ lib/models/store.dart
@@ -0,0 +1,9 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class StoreSimple {
+  int id;
+  String name;
+  LatLng latLng;
+
+  StoreSimple({required this.id, required this.name, required this.latLng});
+}
