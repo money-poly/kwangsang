dcf7c746350120a47c59ff7c86c7c2363a9ab9c9
민감자 <mouse0429@naver.com>
2023-11-22 17:23:01 +0900

    feat(store_tag): 가게 태그를 위한 태그 모델

---
+++ lib/models/store_tag.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class StoreTag {
+  String icon;
+  String name;
+  Color color;
+
+  StoreTag({required this.icon, required this.name, required this.color});
+}
