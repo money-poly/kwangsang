1deef8e028053d1f525017c832781f46217770c2
민감자 <mouse0429@naver.com>
2024-01-01 21:46:30 +0900

    feat(onboarding): 온보딩 화면

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,22 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:immersion_kwangsang/providers/position_provider.dart';
 import 'package:immersion_kwangsang/screens/navigation/nav_view.dart';
 import 'package:immersion_kwangsang/screens/navigation/nav_view_model.dart';
+import 'package:immersion_kwangsang/screens/navigation/onboarding.dart';
 import 'package:immersion_kwangsang/styles/theme.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
-  runApp(const MyApp());
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  // runApp(MyApp(isVisited: false));
+  runApp(MyApp(isVisited: prefs.getBool("visited") ?? false));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, required this.isVisited});
 
+  final bool isVisited;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,7 +29,7 @@ class MyApp extends StatelessWidget {
           ChangeNotifierProvider(create: (_) => NavViewModel()),
           ChangeNotifierProvider(create: (_) => PositionProvider())
         ],
-        child: const NavView(),
+        child: isVisited ? const NavView() : const OnBoarding(),
       ),
     );
   }
---
+++ lib/screens/navigation/onboarding.dart
@@ -0,0 +1,120 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:immersion_kwangsang/main.dart';
+import 'package:immersion_kwangsang/styles/color.dart';
+import 'package:immersion_kwangsang/styles/txt.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class OnBoarding extends StatelessWidget {
+  const OnBoarding({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List titles = ["마감할인도 놓치지 말고", "내 손에서 시작하는 경제적인 소비", "점주와 학생이 공생하는"];
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CarouselSlider(
+              options: CarouselOptions(
+                height: 400.0,
+                autoPlay: true,
+                autoPlayInterval: const Duration(seconds: 3),
+                autoPlayAnimationDuration: const Duration(milliseconds: 800),
+                autoPlayCurve: Curves.fastOutSlowIn,
+                pauseAutoPlayOnTouch: true,
+                viewportFraction: 1,
+              ),
+              items: List.generate(
+                3,
+                (idx) => SizedBox(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        "${titles[idx]}",
+                        style: KwangStyle.btn1SB
+                            .copyWith(color: KwangColor.grey700),
+                      ),
+                      const SizedBox(height: 16),
+                      SizedBox(
+                        height: 80,
+                        child: SvgPicture.asset(
+                          "assets/icons/ic_265_onboarding_${idx + 1}.svg",
+                          width: 265,
+                        ),
+                      ),
+                      const SizedBox(height: 14),
+                      Image.asset(
+                          "assets/imgs/img_346_onboarding_${idx + 1}.png",
+                          width: 346)
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomSheet: Wrap(
+        crossAxisAlignment: WrapCrossAlignment.center,
+        children: [
+          GestureDetector(
+            onTap: () async {
+              await SharedPreferences.getInstance().then((value) {
+                value.setBool("visited", true);
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                      builder: (context) => const MyApp(isVisited: true)),
+                );
+              });
+            },
+            child: Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.only(bottom: 4),
+              height: 44,
+              width: MediaQuery.of(context).size.width,
+              child: Text("건너뛰기",
+                  style: KwangStyle.btn1SB.copyWith(color: KwangColor.grey600)),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(
+                20, 11, 20, 11 + MediaQuery.of(context).viewPadding.bottom),
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              border: Border(
+                top: BorderSide(width: 1.0, color: KwangColor.grey300),
+              ),
+            ),
+            child: GestureDetector(
+              onTap: () async {
+                await SharedPreferences.getInstance().then((value) {
+                  value.setBool("visited", true);
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                        builder: (context) => const MyApp(isVisited: true)),
+                  );
+                });
+              },
+              child: Container(
+                height: 44,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: KwangColor.primary400,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Text("시작하기",
+                    style: KwangStyle.btn2B.copyWith(color: Colors.white)),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
