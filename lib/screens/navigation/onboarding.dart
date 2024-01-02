af73e535df24a112fa0e9f27174ddedbf5e98867
민감자 <mouse0429@naver.com>
2024-01-02 22:06:26 +0900

    fix(onboarding): 온보딩 페이지네이션 추가 및 위치 정보 초기화 추가

--- lib/screens/navigation/onboarding.dart
+++ lib/screens/navigation/onboarding.dart
@@ -2,8 +2,10 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:immersion_kwangsang/main.dart';
+import 'package:immersion_kwangsang/screens/navigation/onboarding_view_model.dart';
 import 'package:immersion_kwangsang/styles/color.dart';
 import 'package:immersion_kwangsang/styles/txt.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class OnBoarding extends StatelessWidget {
@@ -11,54 +13,74 @@ class OnBoarding extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final viewModel = Provider.of<OnBoardingViewModel>(context);
     List titles = ["마감할인도 놓치지 말고", "내 손에서 시작하는 경제적인 소비", "점주와 학생이 공생하는"];
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            CarouselSlider(
-              options: CarouselOptions(
-                height: 400.0,
-                autoPlay: true,
-                autoPlayInterval: const Duration(seconds: 3),
-                autoPlayAnimationDuration: const Duration(milliseconds: 800),
-                autoPlayCurve: Curves.fastOutSlowIn,
-                pauseAutoPlayOnTouch: true,
-                viewportFraction: 1,
-              ),
-              items: List.generate(
-                3,
-                (idx) => SizedBox(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        "${titles[idx]}",
-                        style: KwangStyle.btn1SB
-                            .copyWith(color: KwangColor.grey700),
-                      ),
-                      const SizedBox(height: 16),
-                      SizedBox(
-                        height: 80,
-                        child: SvgPicture.asset(
-                          "assets/icons/ic_265_onboarding_${idx + 1}.svg",
-                          width: 265,
-                        ),
-                      ),
-                      const SizedBox(height: 14),
-                      Image.asset(
-                          "assets/imgs/img_346_onboarding_${idx + 1}.png",
-                          width: 346)
-                    ],
-                  ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Wrap(
+            direction: Axis.horizontal,
+            spacing: 6,
+            children: List.generate(
+              3,
+              (idx) => Container(
+                width: 6,
+                height: 6,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: viewModel.currIdx == idx
+                      ? KwangColor.primary400
+                      : KwangColor.grey500,
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+          const SizedBox(height: 48),
+          CarouselSlider(
+            options: CarouselOptions(
+              height: 400.0,
+              autoPlay: true,
+              autoPlayInterval: const Duration(seconds: 3),
+              autoPlayAnimationDuration: const Duration(milliseconds: 800),
+              autoPlayCurve: Curves.fastOutSlowIn,
+              pauseAutoPlayOnTouch: true,
+              viewportFraction: 1,
+              onPageChanged: (idx, reason) {
+                viewModel.setCurrentPage(idx);
+              },
+            ),
+            items: List.generate(
+              3,
+              (idx) => SizedBox(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "${titles[idx]}",
+                      style:
+                          KwangStyle.btn1SB.copyWith(color: KwangColor.grey700),
+                    ),
+                    const SizedBox(height: 16),
+                    SizedBox(
+                      height: 80,
+                      child: SvgPicture.asset(
+                        "assets/icons/ic_265_onboarding_${idx + 1}.svg",
+                        width: 265,
+                      ),
+                    ),
+                    const SizedBox(height: 14),
+                    Image.asset("assets/imgs/img_346_onboarding_${idx + 1}.png",
+                        width: 346)
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: MediaQuery.of(context).viewPadding.bottom + 110),
+        ],
       ),
       bottomSheet: Wrap(
         crossAxisAlignment: WrapCrossAlignment.center,
