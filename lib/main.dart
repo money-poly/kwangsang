7d47c94b1e0c4e1a3ff0c99f547ef4277d8460e7
민감자 <mouse0429@naver.com>
2024-01-08 13:56:37 +0900

    fix(main): 스플래시 화면에서 위치 권한 요청

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:immersion_kwangsang/providers/position_provider.dart';
 import 'package:immersion_kwangsang/screens/navigation/nav_view.dart';
 import 'package:immersion_kwangsang/screens/navigation/nav_view_model.dart';
@@ -10,9 +12,16 @@ import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
+  WidgetsBinding widgetBinding = WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
   final SharedPreferences prefs = await SharedPreferences.getInstance();
-  // runApp(MyApp(isVisited: false));
+  FlutterNativeSplash.preserve(widgetsBinding: widgetBinding);
+  LocationPermission permission = await Geolocator.checkPermission();
+  if (permission != LocationPermission.always &&
+      permission != LocationPermission.whileInUse) {
+    await Geolocator.requestPermission();
+  }
+  FlutterNativeSplash.remove();
   runApp(MyApp(isVisited: prefs.getBool("visited") ?? false));
 }
 
