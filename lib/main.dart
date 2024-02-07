96260dfec6d63150197f6448a3db0b0b2eb3710b
민감자 <mouse0429@naver.com>
2024-02-07 20:21:59 +0900

    chore: styledToast 설정

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,12 @@ import 'package:immersion_kwangsang/screens/navigation/nav_view.dart';
 import 'package:immersion_kwangsang/screens/navigation/nav_view_model.dart';
 import 'package:immersion_kwangsang/screens/navigation/onboarding.dart';
 import 'package:immersion_kwangsang/screens/navigation/onboarding_view_model.dart';
+import 'package:immersion_kwangsang/styles/color.dart';
 import 'package:immersion_kwangsang/styles/theme.dart';
+import 'package:immersion_kwangsang/styles/txt.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_styled_toast/flutter_styled_toast.dart';
 
 void main() async {
   WidgetsBinding widgetBinding = WidgetsFlutterBinding.ensureInitialized();
@@ -36,15 +39,24 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => NavViewModel()),
         ChangeNotifierProvider(create: (_) => PositionProvider()),
       ],
-      child: MaterialApp(
-        title: 'kwangsaeng',
-        theme: KwangTheme.kwangTheme,
-        home: isVisited
-            ? const NavView()
-            : ChangeNotifierProvider(
-                create: (context) => OnBoardingViewModel(context),
-                child: const OnBoarding(),
-              ),
+      child: StyledToast(
+        locale: const Locale("ko", "KR"),
+        textStyle: KwangStyle.body1.copyWith(color: Colors.white),
+        backgroundColor: KwangColor.grey700,
+        borderRadius: BorderRadius.circular(20),
+        toastAnimation: StyledToastAnimation.slideFromBottom,
+        animDuration: const Duration(milliseconds: 200),
+        duration: const Duration(milliseconds: 1500),
+        child: MaterialApp(
+          title: 'kwangsaeng',
+          theme: KwangTheme.kwangTheme,
+          home: isVisited
+              ? const NavView()
+              : ChangeNotifierProvider(
+                  create: (context) => OnBoardingViewModel(context),
+                  child: const OnBoarding(),
+                ),
+        ),
       ),
     );
   }
