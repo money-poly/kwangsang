4aae6d72158674211bfffff08be9ff56574dfe98
민감자 <mouse0429@naver.com>
2024-05-21 06:04:59 +0900

    fix(textfield_title): 필수 마크 추가

--- lib/widgets/textfield_title.dart
+++ lib/widgets/textfield_title.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:immersion_kwangsang/styles/color.dart';
 import 'package:immersion_kwangsang/styles/txt.dart';
 
 class TextFieldTitle extends StatelessWidget {
-  const TextFieldTitle({super.key, required this.title});
+  const TextFieldTitle(
+      {super.key, required this.title, this.isNecessary = false});
 
   final String title;
+  final bool isNecessary;
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.symmetric(vertical: 12),
       margin: const EdgeInsets.only(bottom: 8),
-      child: Text(title, style: KwangStyle.header3),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(title, style: KwangStyle.header3),
+          if (isNecessary)
+            Text(
+              "*",
+              style: KwangStyle.header3.copyWith(color: KwangColor.red),
+            )
+        ],
+      ),
     );
   }
 }
