e35e9fa39e1aa8ee08ec4dd1dd2adaec660d4704
민감자 <mouse0429@naver.com>
2024-05-21 11:23:13 +0900

    fix(count_to_color): 수량 to 색상 함수 분리

--- lib/widgets/count_tag_widget.dart
+++ lib/widgets/count_tag_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:immersion_kwangsang/styles/color.dart';
 import 'package:immersion_kwangsang/styles/txt.dart';
+import 'package:immersion_kwangsang/utils/count_to_color.dart';
 
 enum CountColorType { background, txt }
 
@@ -13,39 +13,14 @@ class CountTagWidget extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
       decoration: BoxDecoration(
-          color: countToColor(CountColorType.background),
+          color: countToColor(count, CountColorType.background),
           borderRadius: BorderRadius.circular(12)),
       child: Text(
         "$count개 남음",
         style: KwangStyle.btn4.copyWith(
-          color: countToColor(CountColorType.txt),
+          color: countToColor(count, CountColorType.txt),
         ),
       ),
     );
-  }
-
-  Color countToColor(CountColorType type) {
-    final List<int> datums = [4, 7];
-
-    switch (type) {
-      case CountColorType.background:
-        if (count < datums[0]) {
-          return KwangColor.lightRed;
-        } else if (count < datums[1]) {
-          return KwangColor.lightOrange;
-        } else {
-          return KwangColor.lightGreen;
-        }
-      case CountColorType.txt:
-        if (count < datums[0]) {
-          return KwangColor.red;
-        } else if (count < datums[1]) {
-          return KwangColor.orange;
-        } else {
-          return KwangColor.green;
-        }
-      default:
-        return Colors.black;
-    }
   }
 }
