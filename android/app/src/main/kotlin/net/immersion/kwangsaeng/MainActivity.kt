c79f291ae532c2630cf9926b2462c7ecffc7dc42
wjlee611 <wjlee611@gmail.com>
2024-06-19 23:24:52 +0900

    feat: add schemes
    결제 딥링크를 위한 스킴 등록 완료했습니다.
    하지만, 실제로 딥링크 기능이 동작하는지는 확인하지 못했습니다.

--- android/app/src/main/kotlin/net/immersion/kwangsaeng/MainActivity.kt
+++ android/app/src/main/kotlin/net/immersion/kwangsaeng/MainActivity.kt
@@ -1,6 +1,60 @@
 package net.immersion.kwangsaeng
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.KeyData.CHANNEL
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
+import java.net.URISyntaxException
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+        
+        val handler = MethodChannel.MethodCallHandler { call, result ->
+            if (call.method == "getAppUrl") {
+                try {
+                    val url: String = call.argument("url")!!
+                    startSchemeIntent(url)
+                } catch (e: URISyntaxException) {
+                    result.notImplemented()
+                } catch (e: ActivityNotFoundException) {
+                    result.notImplemented()
+                }
+            }
+        }
+
+        val channel = MethodChannel(flutterEngine.dartExecutor, CHANNEL)
+        channel.setMethodCallHandler(handler)
+    }
+
+    private fun startSchemeIntent(url: String): Boolean {
+        val schemeIntent: Intent = try {
+            Intent.parseUri(url, Intent.URI_INTENT_SCHEME) // Intent 스킴을 파싱
+        } catch (e: URISyntaxException) {
+            return false
+        }
+        try {
+            startActivity(schemeIntent) // 앱으로 이동
+            return true
+        } catch (e: ActivityNotFoundException) { // 앱이 설치 안 되어 있는 경우
+            val packageName = schemeIntent.getPackage()
+
+            if (!packageName.isNullOrBlank()) {
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("market://details?id=$packageName") // 스토어로 이동
+                    )
+                )
+                return true
+            }
+        }
+        return false
+    }
 }
